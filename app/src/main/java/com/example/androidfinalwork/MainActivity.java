90e482cf11c9dae5b53e7898bd5f775c8cc70163
LoneYu <576440824@qq.com>
2021-06-10 20:51:25 +0800

    Clone from local gitee repository

---
+++ app/src/main/java/com/example/androidfinalwork/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.androidfinalwork;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<Msg> msgList = new ArrayList<>();
+    private EditText editText;
+    private Button button;
+    private RecyclerView recyclerView;
+    private MsgAdapter adapter;
+    private boolean Send = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("this is for test");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.input_text);
+        button = (Button) findViewById(R.id.send);
+        recyclerView = (RecyclerView) findViewById(R.id.msg_recycler_view);
+        adapter = new MsgAdapter(msgList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = editText.getText().toString();
+                if (!"".equals(content)) {
+                    Msg msg;
+                    if (Send) {
+                        msg = new Msg(content, Msg.TYPE_SEND);
+                    } else {
+                        msg = new Msg(content, Msg.TYPE_RECEIVED);
+                    }
+                    Send = !Send;
+                    msgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size() - 1);
+                    recyclerView.scrollToPosition(msgList.size() - 1);
+                    editText.setText("");
+                }
+            }
+        });
+    }
+}
