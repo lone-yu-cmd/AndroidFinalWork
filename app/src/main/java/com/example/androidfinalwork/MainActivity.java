96984fc1b7951b924dc8c5b5c2e6fc439d01a9ef
KWANLUNG <2544122900@qq.com>
2021-06-11 01:12:55 +0800

    语音识别文字

--- app/src/main/java/com/example/androidfinalwork/MainActivity.java
+++ app/src/main/java/com/example/androidfinalwork/MainActivity.java
@@ -32,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.input_text);
         button = (Button) findViewById(R.id.send);
         recyclerView = (RecyclerView) findViewById(R.id.msg_recycler_view);
-        adapter = new MsgAdapter(msgList);
+
+//        adapter = new MsgAdapter(msgList);
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
@@ -49,7 +51,9 @@ public class MainActivity extends AppCompatActivity {
                         msg = new Msg(content, Msg.TYPE_RECEIVED);
                     }
                     Send = !Send;
-                    msgList.add(msg);
+
+//                    msgList.add(msg);
+
                     adapter.notifyItemInserted(msgList.size() - 1);
                     recyclerView.scrollToPosition(msgList.size() - 1);
                     editText.setText("");
