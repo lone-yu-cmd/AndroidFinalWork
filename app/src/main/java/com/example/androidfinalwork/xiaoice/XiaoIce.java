dff8bf4514736f0a10feda5a58b1dcc23221ffd2
KWANLUNG <2544122900@qq.com>
2021-06-11 14:32:54 +0800

    微软小冰登场 - api成功调用，需要联调

---
+++ app/src/main/java/com/example/androidfinalwork/xiaoice/XiaoIce.java
@@ -0,0 +1,36 @@
+package com.example.androidfinalwork.xiaoice;
+
+import com.example.androidfinalwork.asr.com.baidu.speech.restapi.common.ConnUtil;
+import com.example.androidfinalwork.asr.com.baidu.speech.restapi.common.DemoException;
+import com.example.androidfinalwork.tts.TtsMain;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class XiaoIce {
+    public static void main(String[] args) throws IOException, DemoException {
+        (new XiaoIce()).run();
+    }
+
+//    public final String url = "http://tsn.baidu.com/text2audio"; // 可以使用https
+    public final String url = "http://112.74.185.224:6789/chat"; // 可以使用https
+
+    private void run() throws IOException, DemoException {
+        // 此处2次urlencode， 确保特殊字符被正确编码
+        String params = "text=" + ConnUtil.urlEncode(ConnUtil.urlEncode("今天天气怎样"));
+        params += "&type=" + "text";
+        String URL = url + "?" + params;
+        System.out.println(url + "?" + params); // 反馈请带上此url，浏览器上可以测试
+        HttpURLConnection conn = (HttpURLConnection) new URL(URL).openConnection();
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setConnectTimeout(5000);
+        System.out.println(ConnUtil.getResponseString(conn));
+
+    }
+
+}
