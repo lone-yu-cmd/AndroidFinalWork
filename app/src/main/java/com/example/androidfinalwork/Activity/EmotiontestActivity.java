d0b635304f7a926acb0c17f1655fc17d5eba4896
LoneYu <576440824@qq.com>
2021-06-17 07:05:41 +0800

    改用侧拉栏

---
+++ app/src/main/java/com/example/androidfinalwork/Activity/EmotiontestActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidfinalwork.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidfinalwork.R;
+
+public class EmotiontestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emotiontest);
+    }
+}
